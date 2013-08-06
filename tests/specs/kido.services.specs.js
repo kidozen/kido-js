263f62c29107a987e312d6a71e6fe2147615e49a
Gustavo Machado <machadogj@gmail.com>
2013-08-06 12:59:28 -0300

    adding better error on services test

--- tests/specs/kido.services.specs.js
+++ tests/specs/kido.services.specs.js
@@ -1,51 +1,54 @@
 describe("kido services", function () {
 
-	it("should invoke a method", function ( done ) {
+    it("should invoke a method", function ( done ) {
 
-		new Kido()
-			.services("echo")
-			.invoke("send", { foo: "bar" })
-			.done(function ( data ) {
-				expect(data.foo).to.be.equal("bar");
-				done();
-			})
-			.fail(function (jqXHR, textStatus, errorThrown){
-	            done(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
-			});
-	});
+        new Kido()
+            .services("echo")
+            .invoke("send", { foo: "bar" })
+            .done(function ( data ) {
+                expect(data.foo).to.be.equal("bar");
+                done();
+            })
+            .fail(function (jqXHR, textStatus, errorThrown){
+                done(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
+            });
+    });
 
-	it("should invoke using defaults", function ( done ) {
-		new Kido()
-			.services("echo")
-			.defaults({ kido: "zen" })
-			.invoke("send", { foo: "bar" })
-			.done(function ( data ) {
-				expect(data.foo).to.be.equal("bar");
-				expect(data.kido).to.be.equal("zen");
-				done();
-			})
-			.fail(function (jqXHR, textStatus, errorThrown){
-	            done(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
-			});
+    it("should invoke using defaults", function ( done ) {
+        new Kido()
+            .services("echo")
+            .defaults({ kido: "zen" })
+            .invoke("send", { foo: "bar" })
+            .done(function ( data ) {
+                expect(data.foo).to.be.equal("bar");
+                expect(data.kido).to.be.equal("zen");
+                done();
+            })
+            .fail(function (jqXHR, textStatus, errorThrown){
+                done(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
+            });
 
-	});
+    });
 
-	it("should only re-use the defaults on subsequent calls", function ( done ) {
-		this.timeout(12000);
-		var echo = new Kido().services("echo").defaults({ kido: "zen" });
-		echo
-			.invoke("send", { foo: "bar" })
-			.done(function ( data ) {
-				expect(data.foo).to.be.equal("bar");
-				expect(data.kido).to.be.equal("zen");
-				echo
-					.invoke("send", { hello: "world" })
-					.done(function ( data ) {
-						expect(data.hello).to.be.equal("world");
-						expect(data.kido).to.be.equal("zen");
-						expect(data.foo).to.be.equal(undefined);
-						done();
-					});
-			});
-	});
+    it("should only re-use the defaults on subsequent calls", function ( done ) {
+        this.timeout(12000);
+        var echo = new Kido().services("echo").defaults({ kido: "zen" });
+        echo
+            .invoke("send", { foo: "bar" })
+            .pipe(function ( data ) {
+                expect(data.foo).to.be.equal("bar");
+                expect(data.kido).to.be.equal("zen");
+                return echo
+                    .invoke("send", { hello: "world" })
+                    .done(function ( data ) {
+                        expect(data.hello).to.be.equal("world");
+                        expect(data.kido).to.be.equal("zen");
+                        expect(data.foo).to.be.equal(undefined);
+                        done();
+                    });
+            })
+            .fail(function (jqXHR, textStatus, errorThrown){
+                done(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
+            });
+    });
 });
