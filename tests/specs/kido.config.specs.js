56f5409f194b6bb1e4d7aab7ce29c6998cfb0de0
Gustavo Machado <machadogj@gmail.com>
2013-11-07 19:00:59 -0300

    refactoring configuration unit tests.

--- tests/specs/kido.config.specs.js
+++ tests/specs/kido.config.specs.js
@@ -1,109 +1,98 @@
 describe("kido config", function () {
 
-	this.timeout(10000);
+    this.timeout(10000);
 
-	it("should get all config values", function ( done ) {
+    function doneWithError(jqXHR, textStatus, errorThrown) {
+        console.log("Oops!");
+        this(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
+    }
 
-		var config = new Kido().config();
+    it("should get all config values", function ( done ) {
 
-		config
-			.set("foo", {name:"foo"})
-			.fail(function (jqXHR, textStatus, errorThrown){
-	            done(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
-			})
-			.done(function () {
+        var config = new Kido().config();
+        //set at least one config value.
+        config
+            .set("foo", {name:"foo"})
+            .fail(doneWithError.bind(done))
+            .done(function () {
+                //get all config values.
+                config
+                    .getAll()
+                    .fail(doneWithError.bind(done))
+                    .done(function (names) {
+                        //assert
+                        expect(names).to.be.an("array");
+                        expect(names.length).to.be.greaterThan(0);
+                        done();
+                    });
+            });
+    });
 
-				config
-					.getAll()
-					.fail(done)
-					.done(function (names) {
+    it("set a string value", function( done ) {
 
-						//assert
-						expect(names).to.be.an("array");
-						expect(names.length).to.be.greaterThan(0);
-						done();
-					});
-			});
-	});
+        var config = new Kido().config();
 
-	it("set a string value", function( done ) {
+        config
+            .set("foo", "bar")
+            .fail(doneWithError.bind(done))
+            .done(function () {
 
-		var config = new Kido().config();
+                config
+                    .get("foo")
+                    .fail(done)
+                    .done(function ( foo ) {
+                        expect(foo).to.be.ok();
+                        expect(foo).to.be.equal("bar");
+                        done();
+                    });
+            });
+    });
 
-		config
-			.set("foo", "bar")
-			.fail(function (jqXHR, textStatus, errorThrown){
-	            done(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
-			})
-			.done(function () {
+    it("should set a json object value", function ( done ) {
 
-				config
-					.get("foo")
-					.fail(done)
-					.done(function ( foo ) {
+        var config = new Kido().config();
 
-						expect(foo).to.be.ok();
-						expect(foo).to.be.equal("bar");
-						done();
-					});
-			});
-	});
+        config
+            .set("foo", { a: 2 })
+            .fail(doneWithError.bind(done))
+            .done(function () {
 
-	it("should set a json object value", function ( done ) {
+                config
+                    .get("foo")
+                    .fail(done)
+                    .done(function ( foo ) {
+                        expect(foo).to.be.ok();
+                        expect(foo.a).to.be.equal(2);
+                        done();
+                    });
+            });
+    });
 
-		var config = new Kido().config();
+    it("should delete a value", function ( done ) {
 
-		config
-			.set("foo", { a: 2 })
-			.fail(function (jqXHR, textStatus, errorThrown){
-	            done(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
-			})
-			.done(function () {
+        var config = new Kido().config();
 
-				config
-					.get("foo")
-					.fail(done)
-					.done(function ( foo ) {
+        //create a temp value foo.
+        config
+            .set("foo", "bar")
+            .fail(doneWithError.bind(done))
+            .done(function () {
 
-						expect(foo).to.be.ok();
-						expect(foo.a).to.be.equal(2);
-						done();
-					});
-			});
-	});
+                //delete foo
+                config
+                    .del("foo")
+                    .fail(doneWithError.bind(done))
+                    .done(function () {
 
-	it("should delete a value", function ( done ) {
-
-		var config = new Kido().config();
-
-		//create a temp value foo.
-		config
-			.set("foo", "bar")
-			.fail(function (jqXHR, textStatus, errorThrown){
-	            done(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
-			})
-			.done(function () {
-
-				//delete foo
-				config
-					.del("foo")
-					.fail(function (jqXHR, textStatus, errorThrown){
-			            done(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
-					})
-					.done(function () {
-
-						//make sure it doesn"t exist anymore
-						config
-							.get("foo")
-							.fail(function (jqXHR, textStatus, errorThrown){
-					            done(new Error("Failed with status:"+jqXHR.status+", responseText:"+jqXHR.responseText+", textStatus:"+textStatus+", errorThrown:"+errorThrown));
-							})
-							.done(function ( foo ) {
-
-								expect(foo).to.be.equal(null);
-								done();
-							});
-					});
-			});
-	});
+                        //make sure it doesn"t exist anymore
+                        config
+                            .get("foo")
+                            .fail(doneWithError.bind(done))
+                            .done(function ( foo ) {
+                                expect(foo).to.be.equal(null);
+                                done();
+                            });
+                    });
+            });
+    });
 });
