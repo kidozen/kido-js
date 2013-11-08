d7087656728e5d89e917ad221b8110f1cb4052e0
Gustavo Machado <machadogj@gmail.com>
2013-11-08 15:10:44 -0300

    adding phonegap support

--- kido-js.js
+++ kido-js.js
@@ -609,48 +609,115 @@ wsTrustClient = function(endpointAddress) {
         return token;
     };
 }
-
 /**
  * Kido - Kidozen representation of an Application.
  *
  * Use the Kido class to gain access to all the application's backend services.
  */
-
-var Kido = function() {
+var Kido = function (name, marketplace) {
 
     var self = this;
 
     if (!(this instanceof Kido)) return new Kido();
 
     //leave this for backward compatibility.
-    this.name = 'local';
+    this.name = name || 'local';
+    this.marketplace = marketplace;
+
+    //initialize variables
+    this.local  = this.name === 'local';
+    this.active = !!marketplace;
+    this.authenticated = this.active ? false : true;
+
+    if (this.active) {
+        this.authConfig = $.ajax({
+            url: this.marketplace + "/publicapi/apps?name=" + this.name
+        }).pipe(function (config) {
+            self.url = config[0].url.substring(0, config[0].url.length - 1);
+            return config[0].authConfig;
+        });
+    }
+
+    this.authenticate = function (user, pass, prov) {
+        
+        if (!this.active) throw new Error("No need to authenticate to this Web App");
+        
+        this.token = this.authConfig.pipe(function (config) {
+            var ips = config.identityProviders;
+            prov = prov || Object.keys(ips)[0];
+            var ip = ips[prov];
+
+            if (!ip) throw new Error("Invalid Identity Provider");
+            if (!ip.protocol) throw new Error("Invalid Identity Provider Protocol");
+            if (ip.protocol.toLowerCase() !== "wrapv0.9") throw new Error("Protocol not supported yet.");
+
+            var form = {
+                "wrap_name": user,
+                "wrap_password": pass,
+                "wrap_scope": config.authServiceScope
+            };
+
+            return $.ajax({url: ip.endpoint, data: form, type: 'POST', dataType: 'text'}).pipe(function (body){
+                var assertion = (/<Assertion(.*)<\/Assertion>/.exec(body) || [])[0];
+                if (!assertion) throw new Error("Unable to get a token from IDP");
+                //get a kidozen token
+                var postRequest = {
+                    url : config.authServiceEndpoint,
+                    type : "POST",
+                    data : {
+                        wrap_assertion : assertion,
+                        wrap_scope : config.applicationScope,
+                        wrap_assertion_format : "SAML"
+                    }
+                };
+
+                return $.ajax(postRequest).pipe(function (token) {
+                    self.authenticated = true;
+                    token.token = 'WRAP access_token="' + token.rawToken + '"';
+                    console.log("authenticated");
+                    return token;
+                });
+            });
+        });
+        return this.token;
+    };
 
     /**
      * send an http request to kidozen.
      * @param {object} settings - settings as in jQuery.ajax settings.
      * @api private.
      */
-
     this.send = function ( settings ) {
+        //validate request
+        if (!settings.url)
+            return $.Deferred().reject({ msg: "Not a valid ajax URL.", error: "Invalid URL"});
 
-        settings.url = settings.url
-        settings = $.extend({
+        var defaults = {
             url: "",
             type: "POST",
             cache: true
-        }, settings);
+        };
+        
+        var opts = $.extend({}, defaults, settings);
 
+        if (opts.data && !opts.contentType)
+            opts.contentType = "application/json";
 
-        if(!settings.contentType && settings.data) settings.contentType = "application/json";
-
-        if(!settings.url) {
-            return $.Deferred().reject({ msg: "Not a valid ajax URL.", error: "Invalid URL"});
+        if (self.active && self.authenticated) {
+            return self.token.pipe(function (token) {
+                opts.url = self.url + opts.url;
+                // opts.url = opts.url.replace('https', 'http');
+                opts.headers = opts.headers || {};
+                opts.headers.authorization = token.token;
+                // opts.xhrFields = {
+                //     withCredentials: true
+                // };
+                console.log('opts', opts);
+                return $.ajax(opts);
+            });
         }
-
-        return $.ajax(settings);
-
+        return $.ajax(opts);
     };
-
 
     /**
      * make a GET http request to kidozen.
@@ -659,9 +726,7 @@ var Kido = function() {
      * @param settings {object} - optional settings as in $.ajax settings.
      * @api private
      */
-
     this.get = function ( url, settings ) {
-
         settings = $.extend({ url: url, type: "GET"}, settings || {});
         return self.send(settings);
     };
@@ -675,9 +740,7 @@ var Kido = function() {
      * @param settings {object} - optional settings as in $.ajax. settings.
      * @api private.
      */
-
     this.post = function ( url, data, settings ) {
-
         settings = $.extend({
             url: url,
             type: "POST",
@@ -686,16 +749,13 @@ var Kido = function() {
         return self.send(settings);
     };
 
-
     /**
      * make a DELETE http request to kidozen.
      * @param url {string}      - url for GET.
      * @param settings {object} - optional settings as in $.ajax settings.
      * @api private
      */
-
      this.del = function ( url, settings ) {
-
         settings = $.extend({url: url, type: "DELETE"}, settings || {});
         return self.send(settings);
      };
